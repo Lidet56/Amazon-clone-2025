eb87e4d90691487a5a97f886ca2173fc4920f521
Lidet56 <legesselidet@gmail.com>
2025-12-23 10:47:46 -0500

    deploying on netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-b9613/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-b9613/us-central1/api",
+
+ baseURL: "https://amazon-api-deploy-qtah.onrender.com",
 });
+
 
 export {axiosInstance };
 
